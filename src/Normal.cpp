5299296f01e76529158c37276ca204e0d6553033
Marcus Plutowski <achierius@gmail.com>
2016-01-28 23:32:44 -0800

    Add an Inverse Normal function; changes how the integration works so that dX is dependent on Sigma The latter change should allow us to easily work with PDFs with very small or very large standard deviations. The prior function takes a requested probability and gives the x-value you'd need to get that from the mean.

--- src/Normal.cpp
+++ src/Normal.cpp
@@ -1,9 +1,17 @@
-#include <math.h>
+#include <cmath>
 #include "Normal.h"
+
 float Norm::CDF(Norm::PDF pdf, float min, float max){
+	if(min > max){ //This swaps the two using the XOR swap algorithm
+		min = min ^ max;
+		max = max ^ min;
+		min = min ^ max;
+	}
+
 	float total = 0;
-	for(float i = min; i < max; i+=0.001){
-		total+=(getValue(pdf, i)+getValue(pdf, i+0.001))/2*0.001;
+	float dx = std::get<1>(pdf)/500;
+	for(float i = min; i < max; i+=dx){
+		total+=(getValue(pdf, i)+getValue(pdf, i+dx))/2*dx;
 	}
 	return total;
 }
@@ -11,5 +19,14 @@ float Norm::getValue(Norm::PDF pdf, float value){
 	double e = 2.71828182845904523536;
 	double pi = 3.14159265358979323846;
 	return 1/(std::get<1>(pdf)*sqrt(2*pi))*pow(e, -1*pow(value-std::get<0>(pdf),2)/(2*pow(std::get<1>(pdf),2)));
+}
+float Norm::invNorm(Norm::PDF pdf, float percent){ 
+	if(abs(percent) > 1){percent = percent/abs(percent);}
+	int direction = abs(percent)/percent;
+	float dx = std::get<1>(pdf)/500;
+	for(float i = std::get<0>(pdf); true; i+=dx*direction){
+		if(abs(Norm::CDF(pdf, i < std::get<0>(pdf) ? i : std::get<0>(pdf), i > std::get<0>(pdf ? std::get<0>(pdf) : i) - percent) < 0.001){return i;};
+	}
+	return 0;	 
 }
 
--- src/Normal.h
+++ src/Normal.h
@@ -1,6 +1,8 @@
 #include <tuple>
 namespace Norm{
 	typedef std::tuple<float, float> PDF; //mu, sigma
-	float CDF(PDF pdf, float min, float max);
-	float getValue(PDF pdf, float value);
+
+	float CDF(PDF pdf, float min, float max); //Gives the area / cumulative probability within two bounds on a normal curve
+	float getValue(PDF pdf, float value); //Gives the value of the given normal distribution at any point
+	float invNorm(PDF pdf, float percent) //Starts from centre; if negative, it goes left, if positive, goes right. Determines what position on the curve (starting from the middle) will give you 'percent' area
 }
