aa288ca30da5c3717bd7b3bacd01599d7fac10d3
Marcus Plutowski <achierius@gmail.com>
2016-01-30 13:17:02 -0800

    Add Controller header It's to be extended into FeedForward, FeedBack, and Open controller types, which can then be extended into specific controllers

---
+++ src/Controller.h
@@ -0,0 +1,19 @@
+#ifndef __CONTROLLER_H
+#define __CONTROLLER_H
+
+#include <Eigen/Dense>
+#include "Thruster.h"
+#include <vector>
+
+class Controller{
+public:
+	
+	virtual void useOutput() = 0;
+	virtual void addTarget(Thruster* newTarget) = 0;
+protected:
+	std::vector<Thruster*> targets;
+	
+	
+};
+#endif
+
